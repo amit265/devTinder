6d0a5299af45716b71cf49f40bdb137613dd269c
Amit kumar <amitk.kumar414@gmail.com>
2025-01-10 22:29:45 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello from the server at port 5000")
-})
+app.get("/hello", (req, res) => {
+  res.send("Hello helo hello hello get");
+});
 
-app.use("/hello", (req, res) => {
-    res.send("Hello helo hello hello")
+app.post("/hello", (req, res) => {
+  res.send("hello its amit post");
+});
+
+app.delete("/hello", (req, res) => {
+    res.send("delete hello hello")
 })
 
 app.listen(5000, () => console.log("server listening on 5000"));
